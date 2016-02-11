0f1963aa27d008f8f81a36623846390f958377d8
Julian McClellan <jmcclellan@cs.uchicago.edu>
2016-02-10 18:12:17 -0600

    more work on folder creation and naming

--- testing/folders.py
+++ testing/folders.py
@@ -12,23 +12,39 @@ TEST_PATH_DICT = {'STAT 24400 (Winter 16) Statistical Theory.Method-1' :
 
 TEST_HOME_PATH = os.getcwd()
 
-def create_filepaths(path_dict, cur_path):
+def create_filepaths(dirs_dict, cur_path):
     '''
     Given a starting location and a hierarchy of directory names, this function
-    creates the appropriate directory of 
+    creates the appropriate directory and subdirectories at the start loc.
+
+    Inputs:
+        dirs_dict: a dictionary describing the hierarchy of the directories
+                   and subdirectories
+        cur_path: the filepath to create directories in.  The first cur_path 
+                  before the recursive call is the desired home location of 
+                  the directories in dirs_dict.
+    Outputs:
+        None
     '''
-    
-    for key in path_dict:
-        if not path_dict[key]:
+
+    for key in dirs_dict:
+        if not dirs_dict[key]:
             os.makedirs(cur_path + '/' + key)
         else:
-            create_filepaths(path_dict[key], cur_path + '/' + key)
+            create_filepaths(dirs_dict[key], cur_path + '/' + key)
 
 
 def get_folder_name(proposed_dir_name):
     '''
     Given a proposed directory name, this function replaces any '/' characters 
     (which cannot be part of a directory name) with a '.' character.
+
+    Inputs:
+        proposed_dir_name: a string of the proposed directory name
+
+    Outputs:
+        correct_dir_name: a string of the correct directory name.  Will not 
+                          include a '/' character.
     '''
     correct_dir_name = proposed_dir_name.replace('/', '.')
     return correct_dir_name
