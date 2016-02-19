072150c284de7f13fc6344b509dbe5500c3c9e9b
Andrew Tse Zhu <andyz422@cs.uchicago.edu>
2016-02-19 02:01:12 -0600

    hi

--- chalk_crawler.py
+++ chalk_crawler.py
@@ -4,18 +4,37 @@ import requests
 from robobrowser import RoboBrowser
 import getpass
 
-url = 'https://chalk.uchicago.edu/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1/'
 
-username = input('enter username: ')
-password = getpass.getpass('enter password: ')
+class Chalk_Page:
+    def __init__(self):
+        self.browser = self.login()
 
 
-browser = RoboBrowser(history = True)
-browser.open(url)
-login_form = browser.get_form(action='/webapps/login/')
-login_form['user_id'] = username
-login_form['password'] = password
-browser.session.headers['Referer'] = url
-browser.submit_form(login_form)
-print(str(browser.select('title')))
+    def login(self):
+        url = 'https://chalk.uchicago.edu/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1/'
+
+        username = input('enter username: ')
+        password = getpass.getpass('enter password: ')
+
+        browser = RoboBrowser(history = True)
+        browser.open(url)
+
+        login_form = browser.get_form(action='/webapps/login/')
+        login_form['user_id'] = username
+        login_form['password'] = password
+
+        browser.session.headers['Referer'] = url
+        browser.submit_form(login_form)
+        print(browser.parsed)
+        return browser
+
+
+    # def compile_courses(self):
+    #    for link in self.browser.find_all(
+            
+
+
+
+
+
 
