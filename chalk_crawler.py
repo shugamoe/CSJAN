60a6e340a3fa825caf5a3786531dbed3207ac823
Andrew Tse Zhu <andyz422@cs.uchicago.edu>
2016-02-18 18:50:50 -0600

    poop

--- chalk_crawler.py
+++ chalk_crawler.py
@@ -1,20 +1,21 @@
 #CHALK CRAWLER
 
-from lxml import html
 import requests
-import urllib3
+from robobrowser import RoboBrowser
 import getpass
 
-url = 'https://chalk.uchicago.edu/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1'
+url = 'https://chalk.uchicago.edu/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1/'
 
 username = input('enter username: ')
 password = getpass.getpass('enter password: ')
 
-pool_man = urllib3.PoolManager()
-pool_man.headers['username'] = username
-pool_man.headers['password'] = password
 
-r = pool_man.urlopen('GET', url)
-page = r.read()
+browser = RoboBrowser(history = True)
+browser.open(url)
+login_form = browser.get_form(action='/webapps/login/')
+login_form['user_id'] = username
+login_form['password'] = password
+browser.session.headers['Referer'] = url
+browser.submit_form(login_form)
+print(str(browser.select('title')))
 
-print(page)
