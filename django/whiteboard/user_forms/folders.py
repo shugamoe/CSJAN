739a3f8cd1a7f84cd9a818c30b3ffc2cd2f195db
Julian McClellan <jmcclellan@cs.uchicago.edu>
2016-02-25 11:21:47 -0600

    please work

--- django/whiteboard/user_forms/folders.py
+++ django/whiteboard/user_forms/folders.py
@@ -84,7 +84,8 @@ def get_pdf_paths_and_strings(user):
     thing = subprocess.check_output('pdf2txt.py' + ' ' + "'" + str(pdf) + "'",\
                                       shell=True)
     thing = thing.decode('utf-8')
-    thing = thing.replace('\n', '')
+    thing = re.sub(r'\[\w]{1}', ' ', thing)
+    # thing = thing.replace('\n', '')
     pdf_strings.append(thing)
   return pdf_strings
 
