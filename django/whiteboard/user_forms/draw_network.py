4551949ba4093cf49c6a27e85a4e1aa14249425c
Andrew Lyndon Bonar <anbonar@marowak.cs.uchicago.edu>
2016-03-06 17:38:20 -0600

    added network drawer

---
+++ django/whiteboard/user_forms/draw_network.py
@@ -0,0 +1,42 @@
+import networkx as nx
+import matplotlib as plt
+
+G = nx.Graph()
+
+G.add_node
+
+test_class_1 = Set(	"Abraham, Anand",
+					"Alperin, Elijah",
+					"Bartels, Molly",
+					"Bonar, Andrew",
+					"Carlson, Elsa",
+					"Chiou, Iris",
+					"Clohisy, Luke",
+					"DeGregorio, Jenny"
+					"De Keyser, Olivia",
+					"DeWeever, Gamal",
+					"Fan, Nick",
+					"Fields, Summer",
+					"Hansen, Lorentz",
+					"Hernandez, Steven"
+					"Kurtz, Naomi",
+					"Liu, Zixiong",
+					"Lutz, John",
+					"Majumdar, Shaun",
+					"Mandel, Sydney",
+					"McCarthy, Patrick",
+					"Moore, Claire",
+					"Nichols, William",
+					"Pavlou, Natalia",
+					"Pietraszko, Asia",
+					"Pratt, Juchi",
+					"Quam, Madeline",
+					"Rhee, William",
+					"Sinha, Yash",
+					"Slayton, Aliya",
+					"Sostarics, Thomas",
+					"Thomas, William",
+					"Tu, Vivian",
+					"Turcios, Jose",
+					"Waugh, James",
+					"Zhang, Anqi")
