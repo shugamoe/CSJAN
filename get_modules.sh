0fb4d0de66727a0d818a7606efbece55eced4dbc
Julian McClellan <jmcclellan@cs.uchicago.edu>
2016-03-15 00:43:04 -0500

    Changed stuff

--- get_modules.sh
+++ get_modules.sh
@@ -3,8 +3,8 @@
 # Bash script for installing most modules for Whiteboard.
  
 apt-get update
-pip install Django==1.8.8
-python3 –m pip install django-haystack
+python3 -m pip install Django==1.8.8
+python3 -m pip install django-haystack
 pip install --user Whoosh
 apt-get build-dep python-matplotlib
 apt-get install python3-matplotlib
