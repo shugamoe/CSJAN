dfcfeddac674ece51c62c7b3d432b22510dd5105
Andrew Tse Zhu <andyz422@cs.uchicago.edu>
2016-03-14 21:32:44 -0500

    hi

--- Whiteboard/user_forms/chalk_crawler.py
+++ Whiteboard/user_forms/chalk_crawler.py
@@ -308,7 +308,7 @@ class Courses:
 
                     for student_web_element in list_of_students_web_elements:
                         for professor in professors.split(', ')
-                            prof_str = professor.text.replace(';', '')
+                            prof_str = professor.text.rstrip().replace(';', '')
                             prof_str = professor.split(' ')[1] + ', ' + \
                             professor.split(' ')[0]
 
